975620503124f2ececdb38da8f5fd6e354986e3a
herbertma <hma49@sfu.ca>
2021-04-10 13:15:13 -0700

    added student courses n favorites

--- utils/constants.js
+++ utils/constants.js
@@ -1,3 +1,11 @@
 const LOGIN_ROUTE = "http://localhost:3000/api/v1/auth";
 const ROLE_ROUTE = "http://localhost:3000/api/v1/roles";
-module.exports = { LOGIN_ROUTE, ROLE_ROUTE };
+const FAVORITES_ROUTE = "http://localhost:3000/api/v1/favorites";
+const ENROLLMENTS_ROUTE = "http://localhost:3000/api/v1/enrollments";
+
+module.exports = {
+  LOGIN_ROUTE,
+  ROLE_ROUTE,
+  FAVORITES_ROUTE,
+  ENROLLMENTS_ROUTE,
+};

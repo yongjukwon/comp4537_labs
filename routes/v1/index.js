e46683dc2aa2b1b8d29f359fc0ef3c2a43b1c244
yongjukwon <michaelkyj@gmail.com>
2021-04-10 13:34:46 -0700

    Connect login/register pages with index page

--- routes/v1/index.js
+++ routes/v1/index.js
@@ -2,7 +2,13 @@ const express = require("express");
 const router = express.Router();
 
 router.route("/").get((req, res) => {
-  res.render(__dirname + "../../../static/html/index.html");
+  console.log("TOKEN: ", req.cookies.token);
+  const loggedIn = req.cookies.token;
+  const args = {
+    signUp: loggedIn ? "Log out" : "Sign up",
+  };
+
+  res.render(__dirname + "../../../static/html/index.html", { args: args });
 });
 
 module.exports = router;

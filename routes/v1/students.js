fbe2216e8a1d0111db1cac52f78ee53ca6cc7c6c
herbertma <hma49@sfu.ca>
2021-04-10 12:01:09 -0700

    student page is partially dynamic

--- routes/v1/students.js
+++ routes/v1/students.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const cookie = require("cookie");
 const { getRole } = require("../../utils/roles");
 
+//https://stackoverflow.com/questions/59017101/how-to-create-and-insert-div-element-to-ejs-node-js-express-ejs
+
 router.route("/").get(async (req, res) => {
   if (!req.headers.cookie) {
     res.redirect("login");
@@ -13,7 +15,24 @@ router.route("/").get(async (req, res) => {
   const role = await getRole(cookies.token);
 
   if (role === "STUDENT") {
-    res.render(__dirname + "../../../static/html/student.html");
+    const favorites = [
+      {
+        CourseName: "COMP6969",
+        Description: "Description",
+      },
+    ];
+
+    const enrolled = [
+      {
+        CourseName: "COMP6969",
+        Description: "Description",
+      },
+    ];
+
+    res.render(__dirname + "../../../static/html/student.html", {
+      favorites: favorites,
+      enrolledCourses: enrolled,
+    });
   } else {
     res.redirect("index");
   }
